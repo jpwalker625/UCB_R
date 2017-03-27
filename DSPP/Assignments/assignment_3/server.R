cb4c2ffa3d9ebdbe136e2442f83f2cad2b624eda
Joseph Walker <jpwalker625@gmail.com>
2017-03-27 16:47:37 -0700

    assignment 3 update

--- DSPP/Assignments/assignment_3/server.R
+++ DSPP/Assignments/assignment_3/server.R
@@ -1,6 +1,10 @@
+library(tidyverse)
+library(lubridate)
+library(rgdal)
 library(shiny)
 library(leaflet)
 library(spdplyr)
+
 usa.counties.mapping <- readOGR("gz_2010_us_050_00_20m.json")
 
 cal.counties.mapping <- usa.counties.mapping[usa.counties.mapping@data$STATE == '06', ]
@@ -13,15 +17,57 @@ employment.stats$fips_county<- factor(employment.stats$fips_county)
 
 cal.merged <- merge(cal.counties.mapping, employment.stats, by.x = "COUNTY", by.y = "fips_county", duplicateGeoms = TRUE)
 
+bins <- c(0, 2.7, 6, 9, 12, 15, 18, 21, 24, 27, Inf)
 
 shinyServer(function(input, output){
-  selectedData <- reactive({cal.merged %>% filter(month(period, label = TRUE) == input$months)
+  by.month <- reactive({cal.merged[month(cal.merged$period, label = TRUE) == input$months, ]
     })
   
+    output$county.map <- renderLeaflet({
+      leaflet(cal.merged) %>%
+        addProviderTiles(provider = providers$CartoDB) %>%
+        setView(lng = -119.417931, lat = 36.778259, zoom = 5)
+      })
+    observe({
+      
+      pal <- colorBin(reverse = TRUE, palette = 'RdBu', domain = by.month()$unemployed_rate, bins = bins, pretty = TRUE)
+      
+      labels <-paste(by.month()$NAME," ", by.month()$unemployed_rate)
+      
+      leafletProxy('county.map', data = by.month()) %>%
+        clearShapes() %>%
+        addPolygons(fillColor = ~pal(unemployed_rate),
+                    weight = 2,
+                    opacity = 1,
+                    color = "white",
+                    dashArray = "3",
+                    fillOpacity = .7,
+                    highlight = highlightOptions(weight = 2,
+                                                 fillColor = 'silver',
+                                                 color = 'blue',
+                                                 fillOpacity = 0.7,
+                                                 bringToFront = TRUE),
+                    label = labels,
+                    labelOptions = labelOptions(style = list("font-weight" = "normal",
+                                                              padding = "3px 8px"),
+                                                 textsize = "12px",
+                                                 direction = "auto")
+        ) #end of add Polygons
+       
+      observe({
+          proxy <- leafletProxy("county.map", data = cal.merged)
+          
+          proxy %>% clearControls()
+            pal <- pal
+            proxy %>% addLegend(position = "topright",
+                                pal = pal,
+                                opacity = .7,
+                                values = ~unemployed_rate,
+                                title = paste('Unemployment Rate for', input$months)
+                                )
+      })
   
-  })
-  
-  
-
+    })
+})
   
   
--- DSPP/Assignments/assignment_3/ui.R
+++ DSPP/Assignments/assignment_3/ui.R
@@ -7,11 +7,11 @@ shinyUI(fluidPage(
   titlePanel("California Counties Employment Statistics - 2016"),
   sidebarLayout(
     sidebarPanel(
-      selectInput(inputId = "months", label = "select month", choices = sort(month(cal.merged$period, label = TRUE))
+      selectInput(inputId = "months", label = "select month", choices = sort(month(cal.merged$period, label = TRUE)
                   )# end of select input
-      ), #end of side bar panel
+      )
+      ),#end of side bar panel
       mainPanel("This is where the map goes",
-                textOutput('text'),
-                leafletOutput('counties.map'))
-    )))
+                leafletOutput('county.map'))
+)))
 
