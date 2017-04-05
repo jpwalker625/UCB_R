fe442f4ee2e49a57d21f326f2b88fbb919da4066
jpwalker <jpwalker625@gmail.com>
2017-04-04 22:25:51 -0700

    updating final_project

--- DSPP/final_project/final_project.R
+++ DSPP/final_project/final_project.R
@@ -2,13 +2,29 @@ library(tidyjson)
 library(jsonlite)
 library(tidyverse)
 library(lubridate)
+library(stringr)
 
 df <- stream_in(file("US_expenditure.json"))
 
 
 budget.df <- gather(data = df, key = 'year', value = 'value', 13:73)
 
-budget.df$year <- gsub(pattern = "y", replacement = "", x = budget.df$year)
+budget.df <- within(budget.df, {
+  year <- gsub(pattern = "y", replacement = "", x = year)
+  year <- parse_date(x = year, format = "%Y")
+  year <- year(year)
+  value <- str_replace(string = value, pattern = ",", replacement = "")
+  value <- as.numeric(value)
+  }) 
+  
 
-budget.df$year <- parse_date(x = budget.df$year, format = "%Y")
-budget.df$year <- year(budget.df$year)
+agency.spending <- budget.df %>% 
+  group_by(agency_name, year) %>% 
+  summarise(spending = sum(value, na.rm = TRUE)) %>% 
+  ungroup() %>%
+  group_by(agency_name)
+
+
+ggplot(agency.spending, aes(x = year, y = spending, color = agency_name, group = agency_name)) +
+  geom_point() +
+  geom_line()
