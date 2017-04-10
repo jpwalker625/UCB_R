659d646d977826e02cdf61cea14724c25a802014
Joseph Walker <jpwalker625@gmail.com>
2017-04-10 16:30:52 -0700

    updating final_project folder

--- DSPP/final_project/final_project.R
+++ DSPP/final_project/final_project.R
@@ -1,30 +1,35 @@
-library(tidyjson)
-library(jsonlite)
 library(tidyverse)
 library(lubridate)
-library(stringr)
 
-df <- stream_in(file("US_expenditure.json"))
+df <- read_csv("Fire_Incidents.csv")
 
+source("C:/workspace/UCB_R/DSPP/Assignments/assignment_3/fix_column_names.R")
+#Fix Column Names of DF by converting to snake case
+colnames(df) <- fix_column_names(colnames(df))
 
-budget.df <- gather(data = df, key = 'year', value = 'value', 13:73)
+#cache
+sf.fd <- df
 
-budget.df <- within(budget.df, {
-  year <- gsub(pattern = "y", replacement = "", x = year)
-  year <- parse_date(x = year, format = "%Y")
-  year <- year(year)
-  value <- str_replace(string = value, pattern = ",", replacement = "")
-  value <- as.numeric(value)
-  }) 
-  
-
-agency.spending <- budget.df %>% 
-  group_by(agency_name, year) %>% 
-  summarise(spending = sum(value, na.rm = TRUE)) %>% 
-  ungroup() %>%
-  group_by(agency_name)
+parse.date <- function(x){
+  x <- parse_date_time(x, "%m/%d/%Y %H:%M:%S %p!")
+}
 
 
-ggplot(agency.spending, aes(x = year, y = spending, color = agency_name, group = agency_name)) +
-  geom_point() +
-  geom_line()
+sf.fd <- sf.fd %>% within({
+  incident_date <- parse_date_time(incident_date, "%m/%d/%Y")
+  alarm_dt_tm <- parse.date(alarm_dt_tm)
+  arrival_dt_tm <- parse.date(arrival_dt_tm)
+  close_dt_tm <- parse.date(close_dt_tm)
+})
+
+sf.fd <- sf.fd %>%
+  mutate(response_time = difftime(arrival_dt_tm, alarm_dt_tm, units = "mins"))
+
+
+
+range(sf.fd$response_time, na.rm = TRUE)
+#It appears we have some outliers in the data
+#It appears that there are cases when the unit has arrived on scene before being dispatched which could lead to the negative values we see, as well as many of the 0's.
+#We also see cases where there is a large gap in time between a unit is dispatched and has arrived on the scene.
+
+
